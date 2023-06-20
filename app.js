169737d56c23281c72329b4849e9cae03ff546f3
Naqeeb7506 <92145034+Naqeeb7506@users.noreply.github.com>
2023-06-20 16:56:44 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,59 +1,13 @@
-let small_box = document.querySelectorAll(".small_box a");
-// let box_text = document.querySelectorAll(".box_text");
-let one = document.querySelector(".one");
-let two = document.querySelector(".two");
-let three = document.querySelector(".three");
-let four = document.querySelector(".four");
-let five = document.querySelector(".five");
-let six = document.querySelector(".six");
+let questions = document.querySelectorAll(".question_boxes .box");
+questions.forEach(function (item) {
+  let smallbox = item.querySelector(".small_box");
+  smallbox.addEventListener("click", function () {
+    questions.forEach(function (e) {
+      if (e !== item) {
+        e.classList.remove("show_text");
+      }
+    });
 
-small_box.forEach((item) => {
-  item.addEventListener("click", (e) => {
-    // console.log(item.target.dataset.value);
-    let result = e.target.dataset.value;
-
-    if (result === "1") {
-      one.classList.toggle("no_display");
-      two.classList.add("no_display");
-      three.classList.add("no_display");
-      four.classList.add("no_display");
-      five.classList.add("no_display");
-      six.classList.add("no_display");
-    } else if (result === "2") {
-      two.classList.toggle("no_display");
-      one.classList.add("no_display");
-      three.classList.add("no_display");
-      four.classList.add("no_display");
-      five.classList.add("no_display");
-      six.classList.add("no_display");
-    } else if (result === "3") {
-      three.classList.toggle("no_display");
-      one.classList.add("no_display");
-      two.classList.add("no_display");
-      four.classList.add("no_display");
-      five.classList.add("no_display");
-      six.classList.add("no_display");
-    } else if (result === "4") {
-      four.classList.toggle("no_display");
-      one.classList.add("no_display");
-      two.classList.add("no_display");
-      three.classList.add("no_display");
-      five.classList.add("no_display");
-      six.classList.add("no_display");
-    } else if (result === "5") {
-      five.classList.toggle("no_display");
-      one.classList.add("no_display");
-      two.classList.add("no_display");
-      three.classList.add("no_display");
-      four.classList.add("no_display");
-      six.classList.add("no_display");
-    } else {
-      six.classList.toggle("no_display");
-      one.classList.add("no_display");
-      two.classList.add("no_display");
-      three.classList.add("no_display");
-      four.classList.add("no_display");
-      five.classList.add("no_display");
-    }
+    item.classList.toggle("show_text");
   });
 });
